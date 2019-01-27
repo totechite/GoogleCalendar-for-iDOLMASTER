3fad9a460c3f9cbb3b52474f77681d7fec753ecb
totechite <yutan.h9@gmail.com>
2019-01-28 06:51:58 +0900

    deploy

---
+++ docs/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    base: "/GoogleCalendar-for-iDOLMASTER/",
+    themeConfig: {
+        repo: 'totechite/GoogleCalendar-for-iDOLMASTER',
+        navbar: true,
+        nav: [{
+                text: 'Github',
+                link: 'https://github.com/totechite/GoogleCalendar-for-iDOLMASTER'
+            },
+            {
+                text: '@totechlte',
+                link: 'https://twitter.com/totechlte'
+            },
+        ]
+    }
+}
